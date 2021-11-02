0852a4c612b3a30020fefeac5ef9b3e4f3fec76f
rinkusinghh <rinkusingh05011999@gmail.com>
2021-11-02 13:38:55 +0530

    changed navbar text

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
                     <div className="col-10 mx-auto">
                         <nav className="_navbarBg navbar navbar-expand-lg navbar-light bg-light">
                             <div className="container-fluid">
-                                <NavLink className="navbar-brand _navbarText" to="/">Programmer</NavLink>
+                                <NavLink className="navbar-brand _navbarText" to="/">Coder</NavLink>
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
